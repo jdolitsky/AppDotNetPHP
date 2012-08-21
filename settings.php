7b0f492caca9d5b74f2fcb46ea6a29b5058a6747
Josh Dolitsky <jdolitsky@gmail.com>
2012-08-21 14:59:34 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@
 //
 // 	$app = new AppDotNet();
 //
-$app_clientId     = 'HNRLnJMATwurXxCXQDzkWTHwQqW4NqcP';
-$app_clientSecret = 'LQpUZDRvaZxXxyyknhnmPNzcVXJTSLsa';
-$app_redirectUri  = 'http://btown.does-it.net/callback.php';
+$app_clientId     = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$app_clientSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$app_redirectUri  = 'http://your-website.com/callback.php';
 $app_scope        =  array('stream','email','write_post','follow','messages','export');
